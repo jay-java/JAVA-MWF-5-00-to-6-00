f85a209ed52ccb2faf03e98cd1f7e121a36d42f8
jay-java <jayprajapati.tops@gmail.com>
2024-09-20 18:46:41 +0530

    constrctor

--- CoreJAVA/src/core/P009_Static.java
+++ CoreJAVA/src/core/P009_Static.java
@@ -1,27 +1,33 @@
 package core;
-class Data{
+
+class Data {
 	static {
 		System.out.println("static block in data class");
 	}
 	int id;
 	String name;
 	static String col = "TOPS";
+
 	public void setData() {
-		
+
 	}
+
 	public void getData() {
-		System.out.println("id : "+id+" name : "+name+" col : "+col);
+		System.out.println("id : " + id + " name : " + name + " col : " + col);
 	}
+
 	public static void show() {
-		System.out.println("static fun in data class : "+col);
+		System.out.println("static fun in data class : " + col);
 	}
 }
+
 public class P009_Static {
 	static {
 		System.out.println("static block in main method");
 	}
+
 	public static void main(String[] args) {
-		Data d = new  Data();
+		Data d = new Data();
 		d.getData();
 		Data.show();
 	}
